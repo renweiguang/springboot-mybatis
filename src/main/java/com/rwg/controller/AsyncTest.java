05a0e71752df0d8dc35036628bf87d201085a619
renweiguang <1292263227@qq.com>
2021-04-16 14:59:19 +0800

    【修改代码】

--- src/main/java/com/rwg/controller/AsyncTest.java
+++ src/main/java/com/rwg/controller/AsyncTest.java
@@ -20,6 +20,6 @@ public class AsyncTest
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        System.out.println("完成");
+        System.out.println("完成 啦");
     }
 }
