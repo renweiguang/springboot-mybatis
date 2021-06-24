fefd9bcb11b172bb242481f9026445cfea3e856c
renweiguang <75233607+renweiguang@users.noreply.github.com>
2021-06-24 16:06:24 +0800

    【测试】

--- src/main/java/com/rwg/interceptor/AdminInterceptor.java
+++ src/main/java/com/rwg/interceptor/AdminInterceptor.java
@@ -18,26 +18,25 @@ import javax.servlet.http.HttpServletResponse;
 public class AdminInterceptor implements HandlerInterceptor
 {
     /**
-     * 在请求处理之前进行调用（Controller方法调用之前）
+     * 在请求处理之前进行调用（Controller方法调用之前）****测试提交记录****
      */
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
     {
-//        //需保证调用时，公用同一个httpClient（CloseableHttpClient） 才能保存session，访问此接口
-//        System.out.println("执行了TestInterceptor的preHandle方法");
-//
-//        // 统一拦截（查询当前session是否存在user）(这里user会在每次登陆成功后，写入session)
-//        boolean login = (boolean) request.getSession().getAttribute("USER");
-//        if (login)
-//        {
-//            return true;
-//        }
-//        else
-//        {
-//            return false;// 如果设置为false时，被请求时，拦截器执行到此处将不会继续操作
-//            // 如果设置为true时，请求将会继续执行后面的操作
-        return true;
-//        }
+        //需保证调用时，公用同一个httpClient（CloseableHttpClient） 才能保存session，访问此接口
+        System.out.println("执行了TestInterceptor的preHandle方法");
+
+        // 统一拦截（查询当前session是否存在user）(这里user会在每次登陆成功后，写入session)
+        boolean login = (boolean) request.getSession().getAttribute("USER");
+        if (login)
+        {
+            return true;
+        }
+        else
+        {
+            return false;// 如果设置为false时，被请求时，拦截器执行到此处将不会继续操作
+            // 如果设置为true时，请求将会继续执行后面的操作
+        }
     }
 
     /**
