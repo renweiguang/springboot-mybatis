d276beebc1e128dfffec6105e7214f3fc164e0a9
renwg <https://github.com/renweiguang/springboot-mybatiis.git>
2021-11-03 16:26:35 +0800

    【12312】

--- src/main/java/com/rwg/controller/AsyncTest.java
+++ src/main/java/com/rwg/controller/AsyncTest.java
@@ -16,7 +16,7 @@ public class AsyncTest
     @Async
     public void longtime()
     {
-        System.out.println("我在     执行一项耗时任务");
+        System.out.println("我在         执行一项耗时任务");
         System.out.println("我     在执行一项耗时任务");
         try
         {
