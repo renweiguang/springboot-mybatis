98ee5542b33c7ea0cdbf93c2ce2dae591f5e28d3
renweiguang <1292263227@qq.com>
2023-02-19 17:29:37 +0800

    【renwg】 项目优化

--- src/main/java/com/rwg/controller/AsyncTest.java
+++ src/main/java/com/rwg/controller/AsyncTest.java
@@ -3,26 +3,20 @@ package com.rwg.controller;
 import org.junit.Test;
 import org.springframework.scheduling.annotation.Async;
 
-public class AsyncTest
-{
+public class AsyncTest {
     @Test
-    public void test3() throws Exception
-    {
+    public void test3() throws Exception {
         System.out.println("main函数开始执行");
         longtime();
         System.out.println("新增功能了。。。。。。。。。。。。。。。。");
     }
 
     @Async
-    public void longtime()
-    {
-        System.out.println("我     在执行一项耗时任务");
-        try
-        {
+    public void longtime() {
+        System.out.println("我在执行一项耗时任务");
+        try {
             Thread.sleep(5000);
-        }
-        catch (InterruptedException e)
-        {
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
         System.out.println("完成啦");
--- src/main/java/com/rwg/interceptor/AdminInterceptor.java
+++ src/main/java/com/rwg/interceptor/AdminInterceptor.java
@@ -2,12 +2,11 @@ package com.rwg.interceptor;
 
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- *
- *
  * @Package: com.*.*.interceptor
  * @ClassName: AdminInterceptor
  * @Description:拦截器
@@ -15,28 +14,27 @@ import javax.servlet.http.HttpServletResponse;
  * @date: 2019年9月19日 下午2:20:57
  */
 
-public class AdminInterceptor implements HandlerInterceptor
-{
+public class AdminInterceptor implements HandlerInterceptor {
     /**
      * 在请求处理之前进行调用（Controller方法调用之前）****测试提交记录****
      */
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
-    {
-        //需保证调用时，公用同一个httpClient（CloseableHttpClient） 才能保存session，访问此接口
-        System.out.println("执行了TestInterceptor的preHandle方法");
-
-        // 统一拦截（查询当前session是否存在user）(这里user会在每次登陆成功后，写入session)
-        boolean login = (boolean) request.getSession().getAttribute("USER");
-        if (login)
-        {
-            return true;
-        }
-        else
-        {
-            return false;// 如果设置为false时，被请求时，拦截器执行到此处将不会继续操作
-            // 如果设置为true时，请求将会继续执行后面的操作
-        }
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+//        //需保证调用时，公用同一个httpClient（CloseableHttpClient） 才能保存session，访问此接口
+//        System.out.println("执行了TestInterceptor的preHandle方法");
+//
+//        // 统一拦截（查询当前session是否存在user）(这里user会在每次登陆成功后，写入session)
+//        boolean login = (boolean) request.getSession().getAttribute("USER");
+//        if (login)
+//        {
+//            return true;
+//        }
+//        else
+//        {
+//            return false;// 如果设置为false时，被请求时，拦截器执行到此处将不会继续操作
+//            // 如果设置为true时，请求将会继续执行后面的操作
+//        }
+        return true;
     }
 
     /**
@@ -44,8 +42,7 @@ public class AdminInterceptor implements HandlerInterceptor
      */
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
-            ModelAndView modelAndView)
-    {
+                           ModelAndView modelAndView) {
         System.out.println("执行了TestInterceptor的postHandle方法");
     }
 
@@ -53,8 +50,7 @@ public class AdminInterceptor implements HandlerInterceptor
      * 在整个请求结束之后被调用，也就是在DispatcherServlet 渲染了对应的视图之后执行（主要是用于进行资源清理工作）
      */
     @Override
-    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
-    {
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
         System.out.println("执行了TestInterceptor的afterCompletion方法");
     }
 
